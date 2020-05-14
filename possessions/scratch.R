fd271cbd203b8691a3a2753fa61f2a1d018900de
Ryan Menezes <ryanvmenezes@gmail.com>
2020-05-13 21:36:20 -0700

    save

--- possessions/scratch.R
+++ possessions/scratch.R
@@ -42,8 +42,9 @@ poss.data
 with.ball.summary = poss.data %>%
   group_by(team.name = team.with.ball.name) %>% 
   summarise(
-    ppp = sum(possession.points) / n(),
-    possession.time = mean(possession.time)
+    ppp = sum(possession.points) / n() * 100,
+    possession.time = mean(possession.time),
+    poss.per.game = n() / n_distinct(game.id)
   ) %>% 
   arrange(ppp)
 
@@ -52,7 +53,7 @@ with.ball.summary
 without.ball.summary = poss.data %>%
   group_by(team.name = team.without.ball.name) %>% 
   summarise(
-    ppp = sum(possession.points) / n(),
+    ppp = sum(possession.points) / n() * 100,
     possession.time = mean(possession.time)
   ) %>% 
   arrange(ppp)
@@ -88,4 +89,44 @@ time.model = lm(possession.time ~ team.with.ball.name + team.without.ball.name, 
 
 summary(time.model)
 
-tidy(time.model)
+time.model.team.coefs = tidy(time.model) %>% 
+  transmute(
+    ball.side = case_when(
+      str_detect(term, 'team.with.ball') ~ 'with.ball',
+      str_detect(term, 'team.without.ball') ~ 'without.ball',
+    ),
+    team = str_replace(term, 'team.with.ball.name', ''),
+    team = str_replace(team, 'team.without.ball.name', ''),
+    estimate
+  ) %>% 
+  drop_na(ball.side) %>% 
+  pivot_wider(names_from = 'ball.side', values_from = 'estimate')
+
+time.model.team.coefs  
+
+time.model.team.coefs %>% 
+  pivot_longer(-team, names_to = 'ball.side', values_to = 'estimate') %>% 
+  ggplot(aes(estimate)) +
+  geom_histogram(bins = 100) +
+  facet_wrap(. ~ ball.side, ncol = 1)
+
+
+player.details = poss.data %>% 
+  filter(team.with.ball == 'away') %>% 
+  select(possession.time, possession.points, starts_with('away')) %>% 
+  pivot_longer(-possession.time:-possession.points, names_to = 'player.pos', values_to = 'player.id') %>% 
+  bind_rows(
+    poss.data %>% 
+      filter(team.with.ball == 'home') %>% 
+      select(possession.time, possession.points, starts_with('home')) %>% 
+      pivot_longer(-possession.time:-possession.points, names_to = 'player.pos', values_to = 'player.id')
+  ) %>% 
+  group_by(player.id) %>% 
+  summarise(
+    possessions = n(),
+    ppp = sum(possession.points) / n(),
+    avg.poss.time = mean(possession.time)
+  )
+
+player.details %>% arrange(avg.poss.time) %>% 
+  view()
